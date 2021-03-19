585095b9e9e1c4f2c8f7d9349060906381a46ebc
adavis2010 <78383492+adavis2010@users.noreply.github.com>
2021-03-19 14:37:34 -0400

    Add files via upload

--- form-controls.js
+++ form-controls.js
@@ -1,1 +1,7 @@
 console.log("Ready");
+function button_click(){
+    let inCtrl= document.getElementById("inp");
+    let inValue=inCtrl.value;
+    let outCtrl= document.getElementById("put");
+    outCtrl.value = inValue;
+}
